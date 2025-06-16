99c0b790cae0b7cb69f4300f3d28c46fb31fa543
Duy <duy56236@gmail.com>
2025-06-16 11:10:58 +0700

    refactor: update AppHeader and Home components for improved navigation and data fetching

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,20 @@
-
+'use client';
 import Link from 'next/link'
 import '@/styles/app.css';
 import AppTable from './components/app.table';
+import { useEffect } from 'react';
 export default function Home() {
+
+
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const res = await fetch('http://localhost:8000/blogs');
+      const data = await res.json();
+      console.log(data);
+    }
+    fetchData();
+  }, []);
   return (
     <div>
       <ul>
